8ef1db4a3a311d7b83cd5b4f436a9150fb5375d4
Jacquelinepena <jacquelinepena412@gmail.com>
2016-10-24 11:50:23 -0600

    Lesson 2

---
+++ lesson_2.R
@@ -0,0 +1,10 @@
+# Printing out numbers from 20 to 10 
+for(i in 20:10){ 
+  print(i) } 
+
+# Printing out numbers from 20 to 10 that are even 
+for(i in 20:10){
+  if(i %% 2 == 0) print (i)
+  }
+
+
