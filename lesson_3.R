97690d1b37be680523dc38be17d4afa2ec1abf80
Jacquelinepena <jacquelinepena412@gmail.com>
2016-10-26 11:03:29 -0600

    lesson 3

---
+++ lesson_3.R
@@ -0,0 +1,8 @@
+# Cat class 
+pumpkin <- list(length=4,weight=20,race="bengal")
+class(pumpkin) <- "cat"
+new.dog <- function(weight, breed){
+output <- list(weight=weight, breed=breed)
+class(output) <- "cat"
+  
+ 
