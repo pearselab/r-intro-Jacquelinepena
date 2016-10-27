c1d4c655315ee0dbddc146920a5b8519cfeb4347
Jacquelinepena <jacquelinepena412@gmail.com>
2016-10-27 13:16:46 -0600

    I'm working slowly but surely

--- lesson_2.R
+++ lesson_2.R
@@ -1,10 +1,55 @@
 # Printing out numbers from 20 to 10 
 for(i in 20:10){ 
-  print(i) } 
+  print(i) 
+  } 
 
 # Printing out numbers from 20 to 10 that are even 
 for(i in 20:10){
   if(i %% 2 == 0) print (i)
   }
 
+# Function to calculate whether a number is prime 
+# Google had to help me with this one 
+prime <- function(num) {
+    if (num == 2) {
+      return(TRUE)
+    } else if (any(num %% 2:(num-1) == 0)) {
+      return(FALSE)
+    } else { 
+      return(TRUE)
+    }
+  }
+
+# For loop for divisible # of 5 
+div5 <- function(num) {
+  if(num %% 5 == 0) {
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+}
+
+for(i in 1:20){
+  if(i==5){
+    print("Job: Number")
+  }
+  else if(div5(i)){
+    print("Good: Number")
+  }else if(prime(i)){
+    print("Job: Number")
+  }
+}
+
+# Population Growth 
+# Assign parameters 
+y <- population size
+t <- Time 
+a <- parameter1
+b <- parameter2
+popsize <- function(a,b,c,t){
+y <- a*exp(-b*exp(-c*t))
+return(y)
+} 
+print(popsize(14,35,1000,435))
+print(popsize(1200,15,37,365))
 
--- lesson_3.R
+++ lesson_3.R
@@ -5,4 +5,6 @@ new.dog <- function(weight, breed){
 output <- list(weight=weight, breed=breed)
 class(output) <- "cat"
   
- 
+sequence <- seq(1:20)
+class(point) <- (x=4, y=9)
+
