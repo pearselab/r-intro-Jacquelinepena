edad9f4e5091eff74e5bab9513d127839d7a3779
Jacquelinepena <jacquelinepena412@gmail.com>
2016-10-31 11:19:22 -0600

    #1 doesn't seem quite right

---
+++ lesson_4.R
@@ -0,0 +1,7 @@
+# 1. 
+y <- rep(10,(rnorm(n=1,mean=runif(1,0,10),sd=runif(1,0,10))))
+numeric(0) #What does this mean? I can't tell if this is working 
+# when I change ...mean=runif(1,0,10),sd=runif(1,0,10), I get something different 
+[1] 10 10 10 10
+
+
